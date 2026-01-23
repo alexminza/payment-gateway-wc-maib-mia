e540cde1bb3111eec14b6875f519fdc2ece446d3
Alexander Minza <alexander.minza@gmail.com>
2026-01-23 22:47:52 +0200

    process_payment result message

--- includes/class-wc-gateway-maib-mia.php
+++ includes/class-wc-gateway-maib-mia.php
@@ -524,18 +524,14 @@ class WC_Gateway_MAIB_MIA extends WC_Payment_Gateway_Base
 
         $order->add_order_note($message);
 
-        // https://github.com/woocommerce/woocommerce/issues/48687#issuecomment-2186475264
-        if (WC()->is_store_api_request()) {
-            // phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped -- Message is already escaped.
-            throw new \Exception($message);
-        }
-
         wc_add_notice($message, 'error');
         $this->logs_admin_website_notice();
 
+        // https://github.com/woocommerce/woocommerce/issues/48687#issuecomment-2186475264
+        // https://github.com/woocommerce/woocommerce/pull/53671
         return array(
-            'result'   => 'failure',
-            'messages' => $message,
+            'result'  => 'failure',
+            'message' => $message,
         );
     }
 
