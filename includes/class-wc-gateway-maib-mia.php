ef0205833292ccf420d459e8023125e80f6f4d26
Alexander Minza <alexander.minza@gmail.com>
2026-03-03 20:19:35 +0200

    process_payment return message

--- includes/class-wc-gateway-maib-mia.php
+++ includes/class-wc-gateway-maib-mia.php
@@ -19,11 +19,10 @@ class WC_Gateway_MAIB_MIA extends WC_Payment_Gateway_Base
     const MOD_TEXT_DOMAIN = 'payment-gateway-wc-maib-mia';
     const MOD_PREFIX      = 'maib_mia_';
     const MOD_TITLE       = 'maib MIA';
-    const MOD_VERSION     = '1.1.2';
+    const MOD_VERSION     = '1.1.3';
     const MOD_PLUGIN_FILE = MAIB_MIA_MOD_PLUGIN_FILE;
 
     const SUPPORTED_CURRENCIES = array('MDL');
-    const ORDER_TEMPLATE       = 'Order #%1$s';
 
     const MOD_ACTION_CHECK_PAYMENT = self::MOD_PREFIX . 'check_payment';
 
@@ -524,7 +523,6 @@ class WC_Gateway_MAIB_MIA extends WC_Payment_Gateway_Base
         // https://github.com/woocommerce/woocommerce/pull/53671
         return array(
             'result'  => 'failure',
-            'message' => $message,
         );
     }
 
